bd7cdd1594e7584724c670239f5853d1f378bae1
Minh Quan Le <113287824+Quancoder260503@users.noreply.github.com>
2022-12-06 15:54:17 +1100

    Update Victory.cpp

--- Victory.cpp
+++ Victory.cpp
@@ -10,31 +10,40 @@ ll col[sz];
 int dp[sz],up[20][sz];
 int st[sz],en[sz];
 set<array<int,2>>col_num[sz];
-int tree[sz<<2];
-//Not AC code (need some small update) but have the correct ideas: 
+int bit[sz];
 // for each color we are going to find the lca of (i,i+1) node the color order (Sort by DFS order). 
 // add each lca by -1 and both color node by 1 
 // how does this work --> assume st[i1]<=st[i2]<=st[i3]
 /// --> lca(i1,i2) is the ancestor of lca(i2,i3) or lca(i2,i3) is the ancestor of lca(i1,i2)
 // therefore by updating this way will give corret answer 
 // the remain is just query processing. 
-
-void update(int val,int L,int R,int l=1,int r=n,int p=1){
-    if(L>r or R<l) return;
-    if(L<=l and r<=R){
-        tree[p]+=val;
-        return;
-    }
-    int mid=(l+r)>>1;
-    update(val,L,R,l,mid,2*p);
-    update(val,L,R,mid+1,r,2*p+1);
-    tree[p]=tree[2*p]+tree[2*p+1];
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int sz=5e5+10;
+const int inf=1e6+1;
+int n,k,m,q,add,dis;
+ll timer=1;
+vector<ll>adj[sz];
+ll col[sz];
+int dp[sz],up[20][sz];
+int st[sz],en[sz];
+set<array<int,2>>col_num[sz];
+int bit[sz];
+int sum(int i)
+{
+	int c=0;
+	for(++i; i > 0 ; i -= (i & -i))
+		c+= bit[i];
+	return c;
 }
-int query(int L,int R,int l=1,int r=n,int p=1){
-    if(L>r or R<l) return 0;
-    if(L<=l and r<=R) return tree[p];
-    int mid=(l+r)>>1;
-    return query(L,R,l,mid,2*p)+query(L,R,mid+1,r,2*p+1);
+int query(int l,int r){
+    return sum(r)-sum(l-1);
+}
+void update(int dif,int i)
+{
+	for(++i; i < sz ; i += (i & -i))
+		bit[i] += dif;
 }
 void dfs(int u){
     st[u]=timer++;
@@ -68,14 +77,14 @@ void upd(int e,int add){
                 auto itr=++it; it--;
                 array<int,2>rpos=(*itr);
                 int anc=lca(rpos[1],e);
-                update(add,st[anc],st[anc]);
+                update(add,st[anc]);
           }
           else if(++it==col_num[col[e]].end()){
                  it--;
                  auto itl=--it; it++;
                  array<int,2>lpos=(*itl);
                  int anc=lca(lpos[1],e);
-                 update(add,st[anc],st[anc]);
+                 update(add,st[anc]);
          }
          else if(it!=col_num[col[e]].begin() and it!=col_num[col[e]].end()){
                   auto itr=it; --it;
@@ -85,9 +94,9 @@ void upd(int e,int add){
                   int ancl=lca(e,lpos[1]);
                   int ancr=lca(e,rpos[1]);
                   int anclr=lca(lpos[1],rpos[1]);
-                  update(-add,st[anclr],st[anclr]);
-                  update(add,st[ancr],st[ancr]);
-                  update(add,st[ancl],st[ancl]);
+                  update(-add,st[anclr]);
+                  update(add,st[ancr]);
+                  update(add,st[ancl]);
             }
       }
 }  
@@ -95,7 +104,7 @@ void upd(int e,int add){
 int main(){
     scanf("%d%d%d",&n,&k,&q);
     for(int i=1;i<=n;i++){
-          scanf("%d",col+i);
+          scanf("%lld",col+i);
     }
     for(int i=1;i<n;i++){
         int u,v; scanf("%d%d",&u,&v);
@@ -107,13 +116,13 @@ int main(){
     for(int i=1;i<=k;i++){
          for(auto itr=col_num[i].begin();itr!=col_num[i].end();itr++){
              auto it=itr;
-             array<int,2>up=*it; 
-             update(1,st[up[1]],st[up[1]]);
+             array<int,2>go=*it; 
+             update(1,st[go[1]]);
              if(++it==col_num[i].end()) break;
              array<int,2>vp=*(it); 
-             int u=up[1]; int v=vp[1];
+             int u=go[1]; int v=vp[1];
              int anc=lca(u,v);
-             update(-1,st[anc],st[anc]);
+             update(-1,st[anc]);
          }
     }
     for(int i=1;i<=q;i++){
@@ -129,7 +138,7 @@ int main(){
         else{
             int u; scanf("%d",&u);
             int ans=query(st[u],en[u]);
-            printf("%lld\n",ans);
+            printf("%d\n",ans);
         }
     }
 }
